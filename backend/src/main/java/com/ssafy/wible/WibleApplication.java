20949929c873c35655c231380bb77e0551779dd2
염형덕 <gud_ejr@naver.com>
2022-03-11 11:12:16 +0900

    project create

---
+++ backend/src/main/java/com/ssafy/wible/WibleApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.wible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WibleApplication.class, args);
+	}
+
+}
