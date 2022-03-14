d2c6778aef79bc0785a73a24e9dde5e01706db8c
sangjin <toramx5@gmail.com>
2022-03-14 11:35:21 +0900

    design: 메인페이지 하단부

--- front/src/router/main/Home.js
+++ front/src/router/main/Home.js
@@ -1,5 +1,5 @@
 import logo from "../../res/img/logo.png";
-import { Box, Breadcrumbs, Link } from "@mui/material/";
+import { Box, Breadcrumbs, Link, Grid, Typography, Card, CardActionArea, CardMedia, CardContent } from "@mui/material/";
 
 export default function Home() {
   return (
@@ -35,7 +35,160 @@ export default function Home() {
       </div>
 
       {/* 와인 */}
-      <div></div>
+      <div style={{ marginTop: 50 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8}>
+            {/* 인기 와인 */}
+            <div style={{ marginBottom: 75 }}>
+              <Typography variant="h5" sx={{ mx: 5 }}>
+                지금 인기 있는 와인은?
+              </Typography>
+              <Box sx={{ m: 2, display: "flex", justifyContent: "space-evenly" }}>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea href="/">
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Box>
+            </div>
+            {/* 추천 와인 */}
+            <div style={{ marginBottom: 75 }}>
+              <Typography variant="h5" sx={{ mx: 5 }}>
+                당신만을 위한 와인 추천
+              </Typography>
+              <Box sx={{ m: 2, display: "flex", justifyContent: "space-evenly" }}>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                <Card sx={{ maxWidth: 230, maxHeight: 450 }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="140" image="" alt="와인이미지" />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        와인이름
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구와인설명어쩌구저쩌구
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Box>
+            </div>
+          </Grid>
+          <Grid item xs={2}></Grid>
+        </Grid>
+      </div>
     </>
   );
 }
