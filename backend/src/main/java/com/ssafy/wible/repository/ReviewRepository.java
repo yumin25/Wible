4bac80831cc18491afd301cb063a408393d753eb
kimdy96 <kp03@naver.com>
2022-03-17 10:47:03 +0900

    feat: 댓글 리스트 조회 api 구현 완료

--- backend/src/main/java/com/ssafy/wible/repository/ReviewRepository.java
+++ backend/src/main/java/com/ssafy/wible/repository/ReviewRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.wible.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.wible.model.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer>{
+
+	List<Review> findAllByWineSeq(int wineSeq);
 }
