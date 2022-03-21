12595c1354b3f1817f889529bde80a5a6508db3e
sangjin <toramx5@gmail.com>
2022-03-21 09:44:52 +0900

    design: 설문조사

--- front/src/router/survey/QuestionFive.js
+++ front/src/router/survey/QuestionFive.js
@@ -14,28 +14,28 @@ export default function QuestionFive() {
         선호하는 와인 산지를 골라주십시오(중복가능)
       </Typography>
       <ToggleButtonGroup size="large" value={country} onChange={handleChange} fullWidth sx={{ mb: 3 }}>
-        <ToggleButton value="fra" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="fra" sx={{ width: 290, height: 58 }}>
           프랑스
         </ToggleButton>
-        <ToggleButton value="usa" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="usa" sx={{ width: 290, height: 58 }}>
           미국
         </ToggleButton>
-        <ToggleButton value="ita" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="ita" sx={{ width: 320, height: 58 }}>
           이탈리아
         </ToggleButton>
-        <ToggleButton value="chi" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="chi" sx={{ width: 290, height: 58 }}>
           칠레
         </ToggleButton>
-        <ToggleButton value="por" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="por" sx={{ width: 320, height: 58 }}>
           포르투갈
         </ToggleButton>
-        <ToggleButton value="aus" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="aus" sx={{ width: 290, height: 58 }}>
           호주
         </ToggleButton>
-        <ToggleButton value="esp" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="esp" sx={{ width: 290, height: 58 }}>
           스페인
         </ToggleButton>
-        <ToggleButton value="nzl" sx={{ width: 300, height: 58 }}>
+        <ToggleButton value="nzl" sx={{ width: 320, height: 58 }}>
           뉴질랜드
         </ToggleButton>
       </ToggleButtonGroup>
