4474437edabd041453e3ccdf7ea4597702479233
kimdy96 <kp03@naver.com>
2022-03-22 14:02:47 +0900

    refactor: 와인 리뷰 관련 이름 변경

--- backend/src/main/java/com/ssafy/wible/api/controller/wine/WineController.java
+++ backend/src/main/java/com/ssafy/wible/api/controller/wine/WineController.java
@@ -56,7 +56,7 @@ public class WineController {
 	
 	@ApiOperation(value = "review create", notes = "리뷰 작성, DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PostMapping("/review")
-	public ResponseEntity<String> replyContent(@RequestBody @ApiParam(value = "리뷰 내용.", required = true) ReviewCreateRequest request) {
+	public ResponseEntity<String> reviewCreate(@RequestBody @ApiParam(value = "리뷰 내용.", required = true) ReviewCreateRequest request) {
 		int wineSeq = request.getWineSeq();
 		wineService.wineReviewUpdate(wineSeq);
 		wineService.reviewCreate(request);
@@ -65,7 +65,7 @@ public class WineController {
 	
 	@ApiOperation(value = "review delete", notes = "리뷰 삭제, DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@DeleteMapping("/review")
-	public ResponseEntity<String> replyDelete(@RequestParam @ApiParam(value = "삭제할 댓글번호.", required = true) int reviewSeq) {
+	public ResponseEntity<String> reviewDelete(@RequestParam @ApiParam(value = "삭제할 댓글번호.", required = true) int reviewSeq) {
 		wineService.wineReviewDelete(reviewSeq);
 		wineService.reviewDelete(reviewSeq);
 		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
