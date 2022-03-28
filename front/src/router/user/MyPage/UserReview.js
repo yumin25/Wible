e57805b54b6fb9f866ca85a587932b0ecce97ac1
김수민 <jamie1541@naver.com>
2022-03-28 16:46:46 +0900

    refactor: 검색 디자인 및 기능 수정,url 수정

--- front/src/router/user/MyPage/UserReview.js
+++ front/src/router/user/MyPage/UserReview.js
@@ -7,7 +7,7 @@ import "../../main/Search/Paging.css";
 import { connect } from "react-redux";
 
 function UserReview({ userSlice }) {
-  const url = "http://localhost:8080";
+  const url = "http://j6a303.p.ssafy.io";
   const [page, setPage] = useState(1);
   const [totalCnt, setTotalCnt] = useState(0);
 
