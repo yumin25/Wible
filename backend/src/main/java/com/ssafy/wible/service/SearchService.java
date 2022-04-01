abc2c1d118530cde018fa7019018149aeb26c300
김유민 <elin0225@naver.com>
2022-04-01 14:47:16 +0900

    fix: 와인 검색 api 수정

--- backend/src/main/java/com/ssafy/wible/service/SearchService.java
+++ backend/src/main/java/com/ssafy/wible/service/SearchService.java
@@ -23,7 +23,7 @@ public class SearchService {
 
     public Page<SearchWineResponse> search(String keyword, int start, int end, List<Type> types, List<Integer> body, int tannin, List<Integer> sweet, int acidity, List<Country> countries, Pageable pageRequest){
         start = start * 10000;
-        if(end == 100) end = Integer.MAX_VALUE;
+        if(end >= 100) end = Integer.MAX_VALUE;
         else end = end * 10000;
 
         Specification<Wine> spec = Specification.where(WineSpecification.betweenPrice(start, end));
@@ -33,10 +33,10 @@ public class SearchService {
         }
         if(types.size() != 0) spec = spec.and(WineSpecification.equalsType(types));
         if(countries.size() != 0) spec = spec.and(WineSpecification.equalsCountry(countries));
-        if(body != null) spec = spec.and(WineSpecification.equalsBody(body));
+        if(body.size() != 0) spec = spec.and(WineSpecification.equalsBody(body));
         if(tannin != -1) spec = spec.and(WineSpecification.equalsTannin(tannin));
         if(acidity != -1) spec = spec.and(WineSpecification.equalsAcidity(acidity));
-        if(sweet != null) spec = spec.and(WineSpecification.equalsSweet(sweet));
+        if(sweet.size() != 0) spec = spec.and(WineSpecification.equalsSweet(sweet));
         Page<Wine> wines = wineRepository.findAll(spec, pageRequest);
         Page<SearchWineResponse> result = wines.map(
                 post -> new SearchWineResponse(post.getWineSeq(), post.getKname(), post.getEname(), post.getType(), post.getCountry(), post.getGrapes(), post.getPrice(), post.getImgPath(), post.getScore()));
